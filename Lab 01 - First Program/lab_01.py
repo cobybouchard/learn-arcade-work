5bc5e7c708aee4e65886797541aca4050fbe80fb
Coby <coby.bouchard@my.simpson.edu>
2020-09-02 19:08:58 -0500

    lab1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -5,3 +5,4 @@ print("The hardest part is not even the coding.")
 print("The book said, \"Use at least one escape code,\" so this me trying to do this.")
 print("I absolutely hate this class because I was struggling to even get ready to code, "
       "but now that I am actually coding, I am already enjoying it more.")
+print("Now, I have to figure out how to turn it in.")
