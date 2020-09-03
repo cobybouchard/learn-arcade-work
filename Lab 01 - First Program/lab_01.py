db463100ec39227d106be92068e1828dd5b5b95f
Coby <coby.bouchard@my.simpson.edu>
2020-09-02 19:05:42 -0500

    lab1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,7 @@
+import it as it
+
+print("This is one of the most confusing things I have ever done.")
+print("The hardest part is not even the coding.")
+print("The book said, \"Use at least one escape code,\" so this me trying to do this.")
+print("I absolutely hate this class because I was struggling to even get ready to code, "
+      "but now that I am actually coding, I am already enjoying it more.")
