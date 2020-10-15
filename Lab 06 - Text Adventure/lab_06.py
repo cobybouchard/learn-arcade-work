7a65b3b8bb48f7e4a5a53f6a57aff8da58a80f04
cobybouchard <coby.bouchard@my.simpson.edu>
2020-10-15 00:06:32 -0500

    6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,20 +1,124 @@
-import arcade
-
 class Room:
 
-    def __init__(self):
-        self.description = ""
-        self.north = 0
-        self.east = 0
-        self.south = 0
-        self.west = 0
+    def __init__(self, description, north, east, south, west):
+        self.description = description
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
 
 
 def main():
     room_list = []
+    print("You woke up in a haunted house. You must find your way outside.")
+    # Garage 0
+    room = Room("You are in the garage of the house.",
+                None,
+                1,
+                None,
+                None)
+    room_list.append(room)
+    # South Hall 1
+    room = Room("You are in the south hall. The lights in the room are very dim.",
+                4,
+                2,
+                None,
+                0)
+    room_list.append(room)
+    # Office 2
+    room = Room("You are in the office. The room is very very mess.",
+                5,
+                None,
+                None,
+                1)
+    room_list.append(room)
+    # Living room 3
+    room = Room("You are in the living room. There is a very old TV on but has no signal.",
+                6,
+                4,
+                None,
+                None)
+    room_list.append(room)
+    # Kitchen 4
+    room = Room("You are in the kitchen. You hear a noise, but no one is there.",
+                None,
+                5,
+                1,
+                3)
+    room_list.append(room)
+    # North Hall 5
+    room = Room("You are in the north hall of in a house. The lights in the room are very dim.",
+                8,
+                None,
+                2,
+                4)
+    room_list.append(room)
+    # Bathroom 6
+    room = Room("You are in the bathroom. You look in the mirror and see a man with a knife behind\nyou. When you turn around, he is gone.",
+                None,
+                None,
+                3,
+                None)
+    room_list.append(room)
+    # Deck
+    room = Room("You are on the deck. The stairs are on the north side of the deck.",
+                9,
+                None,
+                None,
+                None)
+    room_list.append(room)
+    # Bedroom 8
+    room = Room("You are in the bedroom. You hear footsteps coming your way. Then enters the man with knife.",
+                None,
+                None,
+                5,
+                7)
+    room_list.append(room)
+
+    current_room = 0
+
     done = False
-    room_list.append()
     while not done:
-    pass
+        print(room_list[current_room].description)
+        user_input = input("What direction do you want to go? ")
+
+        if user_input.lower == "q" or user_input.lower() == "quit":
+            done = True
+
+        elif user_input.lower() == "n" or user_input.lower() == "north":
+            next_room = room_list[current_room].north
+            if next_room is None:
+                print("You cannot go that direction.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "e" or user_input.lower() == "east":
+            next_room = room_list[current_room].east
+            if next_room is None:
+                print("You cannot go that direction.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "s" or user_input.lower() == "south":
+            next_room = room_list[current_room].south
+            if next_room is None:
+                print("You cannot go that direction.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "w" or user_input.lower() == "west":
+            next_room = room_list[current_room].west
+            if next_room is None:
+                print("You cannot go that direction.")
+            else:
+                current_room = next_room
+
+        else:
+            print("The input was not understood.")
+
+        if current_room == 9:
+            print("You won!")
+            done = True
+
 
 main()
---
+++ Lab 08 - Sprites/lab_08.py
@@ -0,0 +1,171 @@
+import arcade
+import random
+
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.5
+SPRITE_SCALING_ROCK = 0.5
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Sprites"
+COIN_COUNT = 50
+ROCK_COUNT = 30
+MOVEMENT_SPEED = 5
+
+
+class Coin(arcade.Sprite):
+    """
+    This class represents the coins on our screen
+    """
+
+    def reset_pos(self):
+        # Reset the coin to a random spot above the screen
+        self.bottom = SCREEN_HEIGHT
+        self.center_x = random.randrange(SCREEN_WIDTH)
+
+    def update(self):
+        # Move the coin
+        self.center_y -= 1
+
+        # Reset the coins to the top after they reach the bottom.
+        if self.top < 0:
+            self.reset_pos()
+
+
+class Rock(arcade.Sprite):
+    """
+    This class represents the rocks on our screen
+    """
+
+    def reset_pos(self):
+        # Reset the rock to a random spot above the screen
+        self.bottom = random.randrange(SCREEN_HEIGHT)
+        self.center_x = SCREEN_WIDTH
+
+    def update(self):
+        # Move the rock
+        self.center_x -= 1
+
+        # Reset the rock to the top after they reach the bottom.
+        if self.right < 0:
+            self.reset_pos()
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self):
+        """
+        Initializer
+        """
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Lab")
+
+        # Sprite lists
+        self.coin_list = None
+        self.player_list = None
+        self.rock_list = None
+
+        # Set up the player
+        self.player_sprite = None
+        self.score = 0
+
+        # Make mouse invisible
+        self.set_mouse_visible(False)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.RED)
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.rock_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
+
+        # Set up the player
+        self.player_sprite = arcade.Sprite("alienBlue_walk1.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+        # Make coins
+        for i in range(COIN_COUNT):
+            # Coin image from kenney.nl
+            coin = Coin("coinGold.png", SPRITE_SCALING_COIN)
+
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+        # Make rocks
+        for i in range(ROCK_COUNT):
+            # Rock image from kenney.nl
+            rock = Rock("rock.png", SPRITE_SCALING_ROCK)
+
+            # Position the rock
+            rock.center_x = random.randrange(SCREEN_WIDTH)
+            rock.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the rock to the lists
+            self.rock_list.append(rock)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.player_list.draw()
+        self.coin_list.draw()
+        self.rock_list.draw()
+
+        # Put the score on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        # Move the Sprite
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+
+        self.coin_list.update()
+        # Check to see if the character hit the coin
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.coin_list)
+
+        # Adding when getting coins
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+
+        self.rock_list.update()
+        # Check to see if the character hit the rock
+        rock_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.rock_list)
+
+        # Adding when getting coins
+        for rock in rock_hit_list:
+            rock.remove_from_sprite_lists()
+            self.score -= 1
+
+
+def main():
+    """ Main method """
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
