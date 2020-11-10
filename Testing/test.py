9db764c311233f26a09a61833bc2fed0eb940d9d
cobybouchard <coby.bouchard@my.simpson.edu>
2020-11-10 00:24:59 -0600

    sorting worksheet

--- Testing/test.py
+++ Testing/test.py
@@ -1,50 +1,93 @@
-import arcade
-
-WIDTH = 20
-HEIGHT = 20
-MARGIN = 5
-ROW_COUNT = 10
-COLUMN_COUNT = 10
-
-SCREEN_HEIGHT = (HEIGHT * ROW_COUNT) + (MARGIN + 1) * ROW_COUNT
-SCREEN_WIDTH = (WIDTH * ROW_COUNT) + (MARGIN + 1) * COLUMN_COUNT
+import random
 
 
-class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+def selection_sort(my_list):
+    """ Sort a list using the selection sort """
+    outer_loop = 0
+    inner_loop = 0
+    # Loop through the entire array
+    for cur_pos in range(len(my_list)):
+        # Find the position that has the smallest number
+        # Start with the current position
+        min_pos = cur_pos
+        outer_loop += 1
+        # Scan left to right (end of the list)
+        for scan_pos in range(cur_pos + 1, len(my_list)):
+            inner_loop += 1
+            # Is this position smallest?
+            if my_list[scan_pos] < my_list[min_pos]:
+                # It is, mark this position as the smallest
+                min_pos = scan_pos
 
-    def __init__(self, width, height):
-        super().__init__(width, height)
-
-        arcade.set_background_color(arcade.color.BLACK)
-
-    def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        arcade.start_render()
-
-        for column in range(COLUMN_COUNT):
-            arcade.draw_rectangle_filled((WIDTH / 2) + (column * (WIDTH + MARGIN)) + MARGIN,
-                                         HEIGHT / 2, WIDTH, HEIGHT, arcade.color.TEAL)
-            for row in range(ROW_COUNT):
-                arcade.draw_rectangle_filled(WIDTH / 2,(HEIGHT / 2) + (row * (HEIGHT + MARGIN)) + MARGIN, WIDTH, HEIGHT, arcade.color.TEAL)
+        # Swap the two values
+        temp = my_list[min_pos]
+        my_list[min_pos] = my_list[cur_pos]
+        my_list[cur_pos] = temp
+    print("Selection Sort Outer loop=", outer_loop)
+    print("Selection Sort Inner loop=", inner_loop)
 
 
-def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
+def insertion_sort(my_list):
+    """ Sort a list using the insertion sort """
+    outer_loop = 0
+    inner_loop = 0
+    # Start at the second element (pos 1).
+    # Use this element to insert into the
+    # list.
+    for key_pos in range(1, len(my_list)):
+        outer_loop += 1
+        # Get the value of the element to insert
+        key_value = my_list[key_pos]
+
+        # Scan from right to the left (start of list)
+        scan_pos = key_pos - 1
+
+        # Loop each element, moving them up until
+        # we reach the position the
+        while (scan_pos >= 0) and (my_list[scan_pos] > key_value):
+            my_list[scan_pos + 1] = my_list[scan_pos]
+            scan_pos = scan_pos - 1
+            inner_loop += 1
+
+        # Everything's been moved out of the way, insert
+        # the key into the correct location
+        my_list[scan_pos + 1] = key_value
+    print("Insertion Sort Outer Loop =", outer_loop)
+    print("Insertion Sort Inner Loop =", inner_loop)
+
+
+# This will point out a list
+# For more information on the print formatting {:3}
+# see the chapter on print formatting.
+def print_list(my_list):
+    for item in my_list:
+        print(f"{item:3}", end="")
+    print()
 
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-    arcade.run()
+    # Create two lists of the same random numbers
+    list_for_selection_sort = []
+    list_for_insertion_sort = []
+    list_size = 5000
+    for i in range(list_size):
+        new_number = random.randrange(100)
+        list_for_selection_sort.append(new_number)
+        list_for_insertion_sort.append(new_number)
+
+    # Print the original list
+    print("Original List")
+    print_list(list_for_selection_sort)
+
+    # Use the selection sort and print the result
+    print("Selection Sort")
+    selection_sort(list_for_selection_sort)
+    print_list(list_for_selection_sort)
+
+    # Use the insertion sort and print the result
+    print("Insertion Sort")
+    insertion_sort(list_for_insertion_sort)
+    print_list(list_for_insertion_sort)
 
 
-if __name__ == "__main__":
-    main()
+main()
